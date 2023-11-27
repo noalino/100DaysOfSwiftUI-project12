c590e7db1d753d615aec1facaf3e718cc1563be9
Noalino <bgelineau.dev@gmail.com>
2023-11-27 11:52:18 +0100

    Create User model

--- SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Noalino on 27/11/2023.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
