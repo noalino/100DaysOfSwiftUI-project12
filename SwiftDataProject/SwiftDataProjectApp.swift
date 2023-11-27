8efa8fb81f84c40074292e02798388c69a392f25
Noalino <bgelineau.dev@gmail.com>
2023-11-27 11:47:24 +0100

    Initial commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Noalino on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
