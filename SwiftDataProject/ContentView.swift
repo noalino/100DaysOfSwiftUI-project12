a6872125b46dfc9a0365ea27edf61f791a9477d9
Noalino <bgelineau.dev@gmail.com>
2023-11-29 15:07:40 +0100

    Add dynamic sorting

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -11,10 +11,14 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var showingUpcomingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)
+    ]
 
     var body: some View {
         NavigationStack {
-            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast)
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
                 .navigationTitle("Users")
                 .toolbar {
                     Button("Add Samples", systemImage: "plus") {
@@ -33,6 +37,22 @@ struct ContentView: View {
 
                     Button(showingUpcomingOnly ? "Show Everyone" : "Show upcoming") {
                         showingUpcomingOnly.toggle()
+                    }
+
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Sort by Name")
+                                .tag([
+                                    SortDescriptor(\User.name),
+                                    SortDescriptor(\User.joinDate)
+                                ])
+
+                            Text("Sort by Join Date")
+                                .tag([
+                                    SortDescriptor(\User.joinDate),
+                                    SortDescriptor(\User.name)
+                                ])
+                        }
                     }
                 }
         }
