1f4b1655b70604f73731779b5d5b85b1967a6a5e
Noalino <bgelineau.dev@gmail.com>
2023-11-30 15:42:56 +0100

    Add User jobs property

--- SwiftDataProject/User.swift
+++ SwiftDataProject/User.swift
@@ -13,7 +13,8 @@ class User {
     var name: String
     var city: String
     var joinDate: Date
-    
+    var jobs = [Job]()
+
     init(name: String, city: String, joinDate: Date) {
         self.name = name
         self.city = city
